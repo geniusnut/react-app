e5b5f12ff285a982b4d1a7fadcb7796e80a9d004
yw07 <wy@shoot.net.cn>
2020-02-11 22:57:13 +0800

    Updates

---
+++ precache-manifest.86896f1e7083fdd7faa650e9d48d71a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d4f4b1bdce7b86f397532a1aadcd6e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "f34091590f2b586bb7e4",
+    "url": "/react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "122082c676db2075f994",
+    "url": "/react-app/static/js/2.343d33a3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-app/static/js/2.343d33a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34091590f2b586bb7e4",
+    "url": "/react-app/static/js/main.0ac19099.chunk.js"
+  },
+  {
+    "revision": "f0669860c960d01684f4",
+    "url": "/react-app/static/js/runtime-main.e442f6a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
